a76f15c9d7dd87e8eab1fbdf85f2bc7148446dd0
xerox78 <xerox78@outlook.pt>
2024-02-12 16:29:23 +0000

    first commit

---
+++ src/test/java/com/xerox78/onlinebookstore/OnlinebookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xerox78.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
